235fbcdb10223535d670a1aff64e3a43d77a1d16
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-11-12 19:02:39 -0300

    feat: make app as responsive

--- src/components/domain/chat/index.tsx
+++ src/components/domain/chat/index.tsx
@@ -1,12 +1,27 @@
+"use client";
+
 import { TextField } from "@/components/ui/text-field";
 import { Message } from "./message";
 
 import styles from "./styles.module.css";
+import { useChatsBar } from "@/components/contexts/chats-bar-context";
+import { Button } from "@/components/ui/button";
+import { PanelLeft } from "lucide-react";
 
 export function Chat() {
+  const { toggleChatsBar, chatsBarIsOpen } = useChatsBar();
+
   return (
     <main className={styles.chat}>
       <header>
+        <Button
+          variant="ghost"
+          data-chat-open={chatsBarIsOpen ? "on" : "off"}
+          onClick={toggleChatsBar}
+          style={{ fontSize: 0 }}
+        >
+          <PanelLeft size={16} />
+        </Button>
         <span>Novo chat</span>
       </header>
       <main>
--- src/components/domain/chats-bar/index.tsx
+++ src/components/domain/chats-bar/index.tsx
@@ -1,14 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 import styles from "./styles.module.css";
+import { useChatsBar } from "@/components/contexts/chats-bar-context";
+import { PanelLeft } from "lucide-react";
 
 export function ChatsBar() {
+  const { chatsBarIsOpen, toggleChatsBar } = useChatsBar();
+
   return (
-    <aside className={styles.chatsBar} data-open="off">
+    <aside
+      className={styles.chatsBar}
+      data-open={chatsBarIsOpen ? "on" : "off"}
+    >
       <div className={styles.wrapper}>
         <header>
           <Button variant="outline">Nova conversa</Button>
-          <Button variant="outline">B</Button>
+          <Button
+            variant="outline"
+            onClick={toggleChatsBar}
+            style={{ fontSize: 0 }}
+          >
+            <PanelLeft size={16} />
+          </Button>
         </header>
 
         <main>
