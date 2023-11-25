50687b5184f248d662baef6c77bdb60d5a67bb9a
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-11-25 20:12:45 -0300

    feat: config next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/services/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
+import { getServerSession } from "next-auth/next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { SessionProvider } from "@/components/contexts/session-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,14 +15,18 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider session={session}>{children}</SessionProvider>
+      </body>
     </html>
   );
 }
