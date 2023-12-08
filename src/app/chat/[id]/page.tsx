668fd82cc5f88f24cf40863723120214830f6f71
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-12-08 18:03:00 -0300

    feat: block access to chat page conditionaly

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -2,6 +2,8 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Chat } from "@/components/domain/chat";
 import { prisma } from "@/services/prisma";
 import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import * as uuid from "uuid";
 
 interface ChatPageProps {
   params: {
@@ -12,6 +14,10 @@ interface ChatPageProps {
 export default async function ChatPage(props: ChatPageProps) {
   const session = await getServerSession(authOptions);
 
+  if (!session || !uuid.validate(props.params.id)) {
+    return redirect("/?error=invalid_chat");
+  }
+
   const data = await prisma.chats.findUnique({
     where: {
       id: props.params.id,
@@ -21,6 +27,10 @@ export default async function ChatPage(props: ChatPageProps) {
       messages: true,
     },
   });
+
+  if (!data) {
+    return redirect("/?error=invalid_chat");
+  }
 
   return (
     <Chat
