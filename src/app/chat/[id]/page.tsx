fdc08b795d6479d0aa4babc54559f162f4293627
Jonatan Frederico <jonatanfrederico@gmail.com>
2023-12-08 16:52:52 -0300

    feat: prevent access any chat

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,5 +1,7 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Chat } from "@/components/domain/chat";
 import { prisma } from "@/services/prisma";
+import { getServerSession } from "next-auth";
 
 interface ChatPageProps {
   params: {
@@ -8,9 +10,12 @@ interface ChatPageProps {
 }
 
 export default async function ChatPage(props: ChatPageProps) {
+  const session = await getServerSession(authOptions);
+
   const data = await prisma.chats.findUnique({
     where: {
       id: props.params.id,
+      callerId: session?.user.id,
     },
     include: {
       messages: true,
